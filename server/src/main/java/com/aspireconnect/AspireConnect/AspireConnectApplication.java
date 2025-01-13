2560879231fa6d741ad3ca99eea4fbb388fde879
RAJ TILAK SINGH <87015087+crackerghost@users.noreply.github.com>
2025-01-14 02:28:16 +0530

    added backend

---
+++ server/src/main/java/com/aspireconnect/AspireConnect/AspireConnectApplication.java
@@ -0,0 +1,13 @@
+package com.aspireconnect.AspireConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspireConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspireConnectApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/aspireconnect/AspireConnect/AspireConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspireconnect.AspireConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspireConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
