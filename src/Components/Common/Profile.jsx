f67cb46310020973a11317b81aff86fa89481e1a
RAJ TILAK SINGH <87015087+crackerghost@users.noreply.github.com>
2025-02-01 15:24:08 +0530

    profile update

--- src/Components/Common/Profile.jsx
+++ src/Components/Common/Profile.jsx
@@ -3,7 +3,7 @@ import { DataContext } from "../../App";
 import api from "../Config/axios";
 
 function Profile() {
-  const { userData, setUserData,setDashboard } = useContext(DataContext);
+  const { userData, setUserData, setDashboard } = useContext(DataContext);
   const [profile_pic, setProfilePic] = useState(
     "https://avatar.iran.liara.run/public"
   );
@@ -43,21 +43,29 @@ function Profile() {
     };
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="fixed font-Manrope z-50 bg-gray-700/30 backdrop-blur-sm w-screen h-screen flex justify-center items-center">
-      <div className="w-[60%] p-4 h-[90%] bg-white rounded-lg">
+    <div className="fixed font-Manrope z-50 bg-gray-700/30 backdrop-blur-sm w-screen h-screen flex justify-center items-center ">
+      <div className="w-[60%] p-4 h-[90%] bg-white rounded-lg flex flex-col justify-evenly">
         <div className="flex justify-between items-center">
-          <img onClick={()=>(setDashboard(0))} src="./fback.png" width={"20px"} alt="" srcset="" />
+          <img
+            onClick={() => setDashboard(0)}
+            src="./fback.png"
+            width={"20px"}
+            alt=""
+            srcset=""
+          />
           <h1 className="text-3xl font-bold w-full text-center ">Profile</h1>
           <h1 className="text-xl font-bold text-gray-400 ">
             {userData?.role.toUpperCase()}
           </h1>
         </div>
-
-        <hr className="my-5" />
         <form
-          className="w-full flex flex-col justify-center items-center"
-          action=""
+          className="w-full flex flex-col justify-center h-full overflow-hidden items-center "
+          onSubmit={handleSubmit}
         >
           <div className="flex flex-col gap-4 items-center">
             <img
@@ -80,55 +88,212 @@ function Profile() {
               <p className="text-sm">{userData?.followers || "2006"}</p>
             </div>
           </div>
-          <hr className="my-5" />
-          <h1 className="text-lg text-start p-4 w-full">
-            Personal Information
-          </h1>
-          <div className="text-sm w-full p-4 flex gap-2">
-            <div>
-              <label className="text-gray-400">First Name</label>
-              <input
-                type="text"
-                className="w-full p-2 border border-gray-300 rounded-md"
-                value={userData.fname}
-                onChange={(e) =>
-                  setUserData({ ...userData, fname: e.target.value })
-                }
-              />
-              <label className="text-gray-400">Last Name</label>
-              <input
-                type="text"
-                className="w-full p-2 border border-gray-300 rounded-md"
-                value={userData.lname}
-                onChange={(e) =>
-                  setUserData({ ...userData, lname: e.target.value })
-                }
-              />
-            </div>
-            <div>
-              <label className="text-gray-400">Age</label>
-              <input
-                type="text"
-                className="w-full p-2 border border-gray-300 rounded-md"
-                value={userData.age}
-                placeholder="20"
-                onChange={(e) =>
-                  setUserData({ ...userData, age: e.target.value })
-                }
-              />
-              <label className="text-gray-400">Gender</label>
-              <input
-                type="text"
-                className="w-full p-2 border border-gray-300 rounded-md"
-                value={userData.gender}
-                placeholder="Male , Female , Other"
-                onChange={(e) =>
-                  setUserData({ ...userData, age: e.target.value })
-                }
-              />
-            </div>
+          {/* <hr className="my-5" /> */}
+          <div className="overflow-y-scroll h-[40%] w-full my-2">
+            <h1 className="text-lg text-start w-full my-4 font-medium">
+              Personal Information
+            </h1>
+            {userData.role === "Student" || userData.role === "Mentor" ? (
+              <div className="text-sm w-full p-4 flex flex-col gap-2">
+                <div className="flex w-full gap-2">
+                  <div className="flex flex-col w-[50%]">
+                    <label className="text-gray-400">First Name</label>
+                    <input
+                      required
+                      type="text"
+                      className="w-full p-2 border border-gray-300 rounded-md outline-none"
+                      value={userData.fname}
+                      onChange={(e) =>
+                        setUserData({ ...userData, fname: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div className="flex flex-col w-[50%]">
+                    <label className="text-gray-400">Last Name</label>
+                    <input
+                      required
+                      type="text"
+                      className="w-full p-2 border border-gray-300 rounded-md outline-none"
+                      value={userData.lname}
+                      onChange={(e) =>
+                        setUserData({ ...userData, lname: e.target.value })
+                      }
+                    />
+                  </div>
+                </div>
+                <div className="flex w-full gap-2">
+                  <div className="flex flex-col w-[50%]">
+                    <label className="text-gray-400">Age</label>
+                    <input
+                      required
+                      type="text"
+                      className="w-full p-2 border border-gray-300 rounded-md outline-none"
+                      value={userData.age}
+                      placeholder="Enter age"
+                      onChange={(e) =>
+                        setUserData({ ...userData, age: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div className="flex flex-col w-[50%]">
+                    <label className="text-gray-400 ">Gender</label>
+                    <input
+                      required
+                      type="text"
+                      className="w-full p-2 border border-gray-300 rounded-md outline-none"
+                      value={userData.gender}
+                      placeholder="Male , Female , Other"
+                      onChange={(e) =>
+                        setUserData({ ...userData, gender: e.target.value })
+                      }
+                    />
+                  </div>
+                </div>
+                {userData.role === "Mentor" ? (
+                  <div>
+                    <div className="flex w-full gap-2">
+                      <div className="flex flex-col w-[50%]">
+                        <label className="text-gray-400">Position</label>
+                        <input
+                          required
+                          type="text"
+                          className="w-full p-2 border border-gray-300 rounded-md outline-none"
+                          value={userData.position}
+                          placeholder="Position"
+                          onChange={(e) =>
+                            setUserData({
+                              ...userData,
+                              position: e.target.value,
+                            })
+                          }
+                        />
+                      </div>
+                      <div className="flex flex-col w-[50%]">
+                        <label className="text-gray-400">Skills</label>
+                        <input
+                          required
+                          type="text"
+                          className="w-full p-2 border border-gray-300 rounded-md outline-none"
+                          value={userData.skills}
+                          placeholder="Enter Your Skills"
+                          onChange={(e) =>
+                            setUserData({ ...userData, skills: e.target.value })
+                          }
+                        />
+                      </div>
+                    </div>
+                    <div className="flex w-full gap-2">
+                      <div className="flex flex-col w-[50%]">
+                        <label className="text-gray-400">Experience</label>
+                        <input
+                          required
+                          type="text"
+                          className="w-full p-2 border border-gray-300 rounded-md outline-none"
+                          value={userData.exp}
+                          placeholder="Experience"
+                          onChange={(e) =>
+                            setUserData({ ...userData, exp: e.target.value })
+                          }
+                        />
+                      </div>
+                      <div className="flex flex-col w-[50%]">
+                        <label className="text-gray-400">Sessions</label>
+                        <input
+                          required
+                          type="text"
+                          className="w-full p-2 border border-gray-300 rounded-md outline-none"
+                          value={userData.sessions}
+                          placeholder="Enter sessions taken till now"
+                          onChange={(e) =>
+                            setUserData({
+                              ...userData,
+                              sessions: e.target.value,
+                            })
+                          }
+                        />
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  ""
+                )}
+                <div>
+                  <label className="text-gray-400">Description</label>
+                  <input
+                    required
+                    type="text"
+                    className="w-full p-2 border border-gray-300 rounded-md outline-none"
+                    value={userData.gender}
+                    placeholder="Description about you"
+                    onChange={(e) =>
+                      setUserData({ ...userData, gender: e.target.value })
+                    }
+                  />
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
+            {userData.role === "Company" ? (
+              <div className="flex flex-col gap-2">
+                <div className="flex w-full gap-2">
+                  <div className="flex flex-col w-[50%]">
+                    <label className="text-gray-400">Comapny Name</label>
+                    <input
+                      required
+                      type="text"
+                      className="w-full p-2 border border-gray-300 rounded-md outline-none"
+                      value={userData.compName}
+                      placeholder="Comapny Name"
+                      onChange={(e) =>
+                        setUserData({ ...userData, compName: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div className="flex flex-col w-[50%]">
+                    <label className="text-gray-400">Company address</label>
+                    <input
+                      required
+                      type="text"
+                      className="w-full p-2 border border-gray-300 rounded-md outline-none"
+                      value={userData.compAddress}
+                      placeholder="Enter Company address"
+                      onChange={(e) =>
+                        setUserData({
+                          ...userData,
+                          compAdress: e.target.value,
+                        })
+                      }
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label className="text-gray-400">Comapany Description</label>
+                  <input
+                    required
+                    type="text"
+                    className="w-full p-2 border border-gray-300 rounded-md outline-none"
+                    value={userData.description}
+                    placeholder="Description about your Comapany"
+                    onChange={(e) =>
+                      setUserData({ ...userData, description: e.target.value })
+                    }
+                  />
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
         </form>
+        <div className=" flex justify-end ">
+          <button
+            type="submit"
+            className="bg-[#FF8C42] p-2 text-white rounded-lg px-4"
+          >
+            Save
+          </button>
+        </div>
       </div>
     </div>
   );
