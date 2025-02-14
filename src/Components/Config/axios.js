f5a8008ddfa75555539e76d022df25798bca97ed
RAJ TILAK SINGH <87015087+crackerghost@users.noreply.github.com>
2025-02-15 05:11:13 +0530

    commited api url

--- src/Components/Config/axios.js
+++ src/Components/Config/axios.js
@@ -1,16 +1,16 @@
 // Create a new file: src/config/axios.js
-import axios from 'axios';
+import axios from "axios";
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 // Add request interceptor to add token to all requests
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
